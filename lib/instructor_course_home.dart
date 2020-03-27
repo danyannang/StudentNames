7b962a600757adddcd1d1f014e2dc27d39e9567a
shankhinda <skhinda@mail.csuchico.edu>
2020-03-26 21:47:06 -0700

    Pictures and names now appear in a grid in view students

--- lib/instructor_course_home.dart
+++ lib/instructor_course_home.dart
@@ -56,7 +56,7 @@ class _InstructorCourseHomeState extends State<InstructorCourseHome> {
               height: MediaQuery.of(context).size.height/5,
               child: RaisedButton(
                   elevation: 25,
-                  onPressed: () => Navigator.of(context).pop(),
+                  onPressed: () => _viewStudents(),
                   child: Text(
                     'View Students',
                   )
@@ -98,5 +98,92 @@ class _InstructorCourseHomeState extends State<InstructorCourseHome> {
     docs.forEach((d){
       print(d.data['Name']);
     });
+  }
+
+  _viewStudents() async{
+    List<String> studentName = new List<String>();
+    List<String> studentPic = new List<String>();
+    docs.forEach((data){
+      studentName.add(data.data['Name']);
+      studentPic.add(data.data['Pic']);
+    });
+    // print(student);
+    Navigator.push(context,MaterialPageRoute(builder: (context) => StudentView(studentName, studentPic)));
+  }
+}
+
+class StudentView extends StatefulWidget {
+  List<String> studentName = new List<String>();
+  List<String> studentPic = new List<String>();
+  StudentView(this.studentName, this.studentPic);
+  @override
+  _StudentViewState createState() => _StudentViewState(studentName, studentPic);
+}
+
+class _StudentViewState extends State<StudentView> {
+  List<String> studentName = new List<String>();
+  List<String> studentPic = new List<String>();
+  _StudentViewState(this.studentName, this.studentPic);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Student View"),),
+      body: Container(
+        color: Colors.black87,
+        child: GridView.count(
+          childAspectRatio: .65,
+          padding: const EdgeInsets.all(4.0),
+          crossAxisCount: 2,
+          children: List.generate(studentName.length, (index){
+            return Card(
+              color: Color.fromRGBO(120, 0, 0, 1),
+              child: ListTile(
+                title: Image.network(studentPic[index],
+                  loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent chunk){
+                    if(chunk == null){
+                      return child;
+                    }
+                    else{
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: chunk.expectedTotalBytes != null ? chunk.cumulativeBytesLoaded / chunk.expectedTotalBytes : null,
+                        )
+                      );
+                    }
+                  },
+                  height: 235,
+                ),
+                subtitle: Text(studentName[index], style: TextStyle(color: Colors.black, fontSize: 18)),
+              ),
+            );
+            
+            // return GridTile(
+            //   child: Column(
+            //     children: <Widget>[
+            //       Image.network(studentPic[index],
+            //         loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent chunk){
+            //           if(chunk == null){
+            //             return child;
+            //           }
+            //           else{
+            //             return Center(
+            //               child: CircularProgressIndicator(
+            //                 value: chunk.expectedTotalBytes != null ? chunk.cumulativeBytesLoaded / chunk.expectedTotalBytes : null,
+            //               )
+            //             );
+            //           }
+            //         },
+            //         fit: BoxFit.cover,
+            //         height: 250,
+            //       ),
+            //       Text(studentName[index].toString())
+            //     ],
+            //   )
+            // );
+          })
+        )
+      )
+    );
   }
 }
